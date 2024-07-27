754f84e760477a197a90562fbae19faddeb1d780
mellinacerqueira@gmail.com <mellinacerqueira@gmail.com>
2024-07-27 19:05:22 -0300

    removed composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
